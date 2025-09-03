9d6d6e1669965884cee66e6b3b4b930d88c895bb
Harsh <harshasnade77@gmail.com>
2025-09-03 16:10:49 +0000

    change the supabase to import { createClient } from '@supabase/supabase-

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://qfhnylafanneyfniuemn.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFmaG55bGFmYW5uZXlmbml1ZW1uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY4NzE2NzEsImV4cCI6MjA3MjQ0NzY3MX0.LyDSiADTZhANVHFCjyqgO1qWFvH9HmqbNwn9z6PB3Uk'
+const supabaseUrl = 'https://avgbzxjwwpxkiebsncey.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2Z2J6eGp3d3B4a2llYnNuY2V5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY4OTU2NjgsImV4cCI6MjA3MjQ3MTY2OH0.M_jlSzSq6yLWmxyhQaBlAtITD3hBPW2TCla1aEc9Of4'
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Supabase URL and anonymous key are required. Make sure to set NEXT_PUBLIC_SUPABASE_URL and NEXT_PUBLIC_SUPABASE_ANON_KEY in your .env.local file.');
