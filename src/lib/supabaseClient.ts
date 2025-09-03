0599835a6f5d5beec0deeb464c728a0265a29ce6
Harsh <harshasnade77@gmail.com>
2025-09-03 10:54:48 +0000

    Project URL https://qfhnylafanneyfniuemn.supabase.co
    Copy
    A RESTful end

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://qfhnylafanneyfniuemn.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFmaG55bGFmYW5uZXlmbml1ZW1uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY4NzE2NzEsImV4cCI6MjA3MjQ0NzY3MX0.LyDSiADTZhANVHFCjyqgO1qWFvH9HmqbNwn9z6PB3Uk'
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Supabase URL and anonymous key are required. Make sure to set NEXT_PUBLIC_SUPABASE_URL and NEXT_PUBLIC_SUPABASE_ANON_KEY in your .env.local file.');
